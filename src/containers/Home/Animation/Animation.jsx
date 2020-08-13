664dad29c30664ccb415be31611622a9cad5628f
Michael CLAUDE <miclaude@e1r3p4.42.fr>
2020-08-13 17:44:57 +0200

    Animation CSS+Sound || Animation Video Valider

--- src/containers/Home/Animation/Animation.jsx
+++ src/containers/Home/Animation/Animation.jsx
@@ -7,9 +7,10 @@ const Animation = () => (
             <span></span>
             <span></span>
         </div>
-        <h3>t'arrete pas d'etre pas d'etre beau HICHTO SIUUUU et tu le sais</h3>
+        <h3>t'arrete pas d'etre beau HICHTO SIUUUU et tu le sais</h3>
+        <audio autoPlay={true} src={process.env.PUBLIC_URL + '/anim.mp3'}/>
     </div>
- 
+    
     // <video className="logo" src={process.env.PUBLIC_URL + '/anim.mp4'} autoPlay={true} type="video/mp4"/>
 )
 
