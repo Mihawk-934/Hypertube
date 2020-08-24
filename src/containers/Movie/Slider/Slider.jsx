2be1db0d0ed2eda828db8b904790b7c1649cce57
Michael CLAUDE <miclaude@e1r4p2.42.fr>
2020-08-24 11:39:35 +0200

    ajout responsive pour Slider

--- src/containers/Movie/Slider/Slider.jsx
+++ src/containers/Movie/Slider/Slider.jsx
@@ -8,18 +8,44 @@ import './Slider.css';
 
 const SimpleSlider = (props) => {
   var settings = {
+    width: '90%',
     dots: true,
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
     autoplay: true,
     autoplaySpeed: 4000,
-    speed: 1000
+    speed: 1000,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   };
 
   return (
-    <>
-      <h3>Films similaires :</h3>
+    <div style={{width:'92%', margin:'auto'}}>
+      <h2 className=' TitleSlider'>Films similaires :</h2>
       <Slider {...settings}>
         {
           props.similar.map(movie => {
@@ -31,7 +57,7 @@ const SimpleSlider = (props) => {
           })
         }
       </Slider>
-    </>
+    </div>
   );
 }
 
