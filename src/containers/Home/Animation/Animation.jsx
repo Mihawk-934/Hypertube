223c992097e2f4a0ea21c2be1fa6119c0b7f3c2d
Mikail Mihawk <miclaude@student.42.fr>
2020-10-08 16:17:42 +0200

    ajout condition animation Netflix pour safari

--- src/containers/Home/Animation/Animation.jsx
+++ src/containers/Home/Animation/Animation.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Animation.css';
 
-const Animation = () => (
-    <video className="video" src={process.env.PUBLIC_URL + '/anim.mp4'} autoPlay={true} type="video/mp4"/>
-)
+const Animation = () => {
+    const video = !!navigator.userAgent.match(/[Ss]afari/)[0] ?
+    ( <video className="video" id="intro" src={process.env.PUBLIC_URL + '/anim.mp4'} autoPlay={true} type="video/mp4"/>)
+    : ( <video className="video" id="intro" muted src={process.env.PUBLIC_URL + '/anim.mp4'} autoPlay={true} type="video/mp4"/>)
+    return (
+        <>{video}</>
+    )
+}
 
 export default Animation;
