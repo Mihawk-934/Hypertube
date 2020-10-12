5dc6dc00126c7ac998b91e7f4018b1a36a70ea32
Michael CLAUDE <miclaude@e2r13p4.42.fr>
2020-10-12 17:31:19 +0200

    tchat Serveur

--- src/hoc/Layout/Tchat/Tchat.jsx
+++ src/hoc/Layout/Tchat/Tchat.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import { Widget} from 'react-chat-widget';
+import React, { useEffect} from 'react';
+import io from 'socket.io-client'
+import { Widget,addResponseMessage} from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
-import './Tchat.css';
+const socket = io("http://localhost:3008")
 
 const Chat = () => {
+
+    useEffect (()=>{
+        socket.on('msg serveur:', msg=> {
+            addResponseMessage(msg)
+        })
+        return () => {
+            socket.off(addResponseMessage);
+        };           
+    },[])
+
+    const handleNewUserMessage = (newMessage) => {
+        socket.emit('msg envoyer :',localStorage.getItem('name') ,newMessage)
+    };
+      
     return(
-        <div className="Chat">
-            <Widget title="Netflix Chat" subtitle={'Profiter'} senderPlaceHolder="ecrire un message" />
+        <div className="chat">
+            <Widget className="rcw-widget-container" title="Tchat"  subtitle={'VIP'} handleNewUserMessage={handleNewUserMessage}  senderPlaceHolder="ecrire un message" />
         </div>
     )
 }
-
 export default Chat;
