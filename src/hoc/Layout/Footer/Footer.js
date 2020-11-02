fca28edd02f27134e2074e78741c6a718ec1527c
Mikail Mihawk <miclaude@student.42.fr>
2020-11-02 16:04:24 +0100

    maj project

--- src/hoc/Layout/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-    <footer className='mb-0 d-flex align-items-center justify-content-center text-white font-italic' style={{height:'80px', backgroundColor:'black', fontFamily:"Oswald"}}>
-        © 2020 Copyright : Miclaude
-    </footer>
-);
-
-export default Footer;
