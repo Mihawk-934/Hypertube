9f82c3123e64444d0a6e3bf9bb13a029470abb7d
Mikail Mihawk <miclaude@student.42.fr>
2020-11-02 17:22:34 +0100

    modif Tchat for servor socket

--- Front/src/hoc/Layout/Tchat/Tchat.jsx
+++ Front/src/hoc/Layout/Tchat/Tchat.jsx
@@ -3,21 +3,21 @@ import io from 'socket.io-client'
 import { Widget,addResponseMessage } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
 
-// const socket = io("http://localhost:3008")
+const socket = io("http://localhost:3008")
 
 const Chat = () => {
 
     useEffect (()=>{
-        // socket.on('msg serveur:', msg=> {
-        //     addResponseMessage(msg)
-        // })
-        // return () => {
-        //     socket.off(addResponseMessage);
-        // };           
+        socket.on('msg serveur:', msg=> {
+            addResponseMessage(msg)
+        })
+        return () => {
+            socket.off(addResponseMessage);
+        };           
     },[])
 
     const handleNewUserMessage = (newMessage) => {
-        // socket.emit('msg envoyer :',localStorage.getItem('name') ,newMessage)
+        socket.emit('msg envoyer :',localStorage.getItem('name') ,newMessage)
     };
       
     return(
